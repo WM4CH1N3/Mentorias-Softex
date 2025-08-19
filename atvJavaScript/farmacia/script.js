e97c5d6bcce529b31ac70f611a91658d30d4dddb
Mateus Lira <mateus.lrg@gmail.com>
2025-08-19 16:39:18 -0300

    atualizando script

--- atvJavaScript/farmacia/script.js
+++ atvJavaScript/farmacia/script.js
@@ -1,3 +1,9 @@
+function truncar(valor, casas) {
+    let fator = Math.pow(10, casas);
+    return Math.floor(valor * fator) / fator;
+}
+
+let totalSoma=0;
 function adicionar(id){
     let qtd= parseInt(prompt("Quantas unidades desse medicamento\ndeseja adcionar ao carrinho?"));
     let texto = document.getElementById("itens-adicionados");
@@ -6,15 +12,22 @@ function adicionar(id){
     //let valorDesconto = valor.toFixed(2);
     let total= document.getElementById("itens-total");
     let soma = (valor*qtd);
-
-    
+    let somaDesconto = truncar(soma,0);
+ 
     if(qtd<1){
         alert("ação inválida")
-    }else{
+    }else if(qtd==1){
         texto.innerHTML +="<strong>Produto:</strong> "+descricao+" &emsp;<strong>Valor Unitário:</strong> R$"+valor+
-        " <br><strong>Quantidade: </strong>"+qtd+" &emsp;<strong>Valor de compra:</strong> R$ "+(soma)+
-        " &emsp;<strong>Valor com desconto:</strong> R$ "+(soma).toFixed(0)+"<br><br>"
-        total.innerHTML="Valor total: "+ soma
+        " <br><strong>Quantidade: </strong>"+qtd+" &emsp;<strong>Valor de compra:</strong> R$ "+truncar(soma,2)+
+        " &emsp;<strong>Valor com desconto:</strong> R$ "+(soma)+"<br><br>"
+        totalSoma+=soma;
+        total.innerHTML="Valor total: "+ truncar(totalSoma,2)
+    }else if(qtd>1){
+        texto.innerHTML +="<strong>Produto:</strong> "+descricao+" &emsp;<strong>Valor Unitário:</strong> R$"+valor+
+        " <br><strong>Quantidade: </strong>"+qtd+" &emsp;<strong>Valor de compra:</strong> R$ "+truncar(soma,2)+
+        " &emsp;<strong>Valor com desconto:</strong> R$ "+somaDesconto+"<br><br>"
+        totalSoma+=somaDesconto;
+        total.innerHTML="<strong>Valor total: R$</strong>"+ truncar(totalSoma,2) 
     }
 
 }
