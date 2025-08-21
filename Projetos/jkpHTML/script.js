613d577b66587ff96bfb048e02c844c165f1378d
Mateus Lira <mateus.lrg@gmail.com>
2025-08-21 01:10:51 -0300

    projeto jkp

---
+++ Projetos/jkpHTML/script.js
@@ -0,0 +1,13 @@
+function jogar(){
+    document.getElementById("tela-inicial").style.display="none"
+    document.getElementById("tela-jogo").style.display="flex"
+}
+
+function escolher(){
+
+}
+
+function sair(){
+    document.getElementById("tela-inicial").style.display="flex"
+    document.getElementById("tela-jogo").style.display="none"
+}
